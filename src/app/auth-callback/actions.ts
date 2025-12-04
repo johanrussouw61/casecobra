b63728fe006b92117fae5300e491e69653de078e
johanrussouw <jrussouw610505@gmail.com>
2025-12-04 20:03:48 +0200

    working on auth-callback

---
+++ src/app/auth-callback/actions.ts
@@ -0,0 +1,31 @@
+"use server";
+
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { db } from "../db";
+
+export const getAuthStatus = async () => {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+  console.log("user", user);
+
+  if (!user?.id || !user.email) {
+    throw new Error("Invalid user data");
+  }
+
+  const existingUser = await db.user.findFirst({
+    where: { id: user.id },
+  });
+
+  console.log("existingUser", existingUser);
+
+  if (!existingUser) {
+    await db.user.create({
+      data: {
+        id: user.id,
+        email: user.email,
+      },
+    });
+  }
+
+  return { success: true };
+};
--- src/app/auth-callback/page.tsx
+++ src/app/auth-callback/page.tsx
@@ -1,4 +1,33 @@
 "use client";
 
-const Page = () => {};
+import { useQuery } from "@tanstack/react-query";
+import { useEffect, useState } from "react";
+import { getAuthStatus } from "./actions";
+import { useRouter } from "next/navigation";
+
+const Page = () => {
+  const [configId, setConfigId] = useState<string | null>(null);
+  const router = useRouter();
+
+  useEffect(() => {
+    const configurationId = localStorage.getItem("configurationId");
+    // eslint-disable-next-line react-hooks/set-state-in-effect
+    if (configurationId) setConfigId(configurationId);
+  }, []);
+
+  const { data } = useQuery({
+    queryKey: ["auth-callback"],
+    queryFn: async () => await getAuthStatus(),
+    retry: true,
+    retryDelay: 1000,
+  });
+
+  if (data?.success) {
+    localStorage.removeItem("configuratinId");
+    router.push(`/configure/preview?id=${configId}`);
+  } else {
+    router.push("/");
+  }
+};
+
 export default Page;
