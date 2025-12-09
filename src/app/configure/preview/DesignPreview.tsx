da4a3a426b14519bdb93e08bb70295b626a482e5
johanrussouw <jrussouw610505@gmail.com>
2025-12-09 13:01:24 +0200

    changed user to geUser in DesignPreview.tsx

--- src/app/configure/preview/DesignPreview.tsx
+++ src/app/configure/preview/DesignPreview.tsx
@@ -115,8 +115,11 @@ const DesignPreview = ({ configuration }: { configuration: Configuration }) => {
   const confettiRef = useRef<HTMLDivElement | null>(null);
   const router = useRouter();
   const { id } = configuration;
-  const { user } = useKindeBrowserClient();
+  const { getUser } = useKindeBrowserClient();
   const [isLoginModalOpen, SetIsLoginModalOpen] = useState(false);
+
+  const user = getUser();
+  console.log("user inside design preview: ", user);
 
   useEffect(() => {
     // trigger once on mount
