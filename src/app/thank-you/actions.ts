17a166efd305e49e1de34167aa203e2743673760
johanrussouw <jrussouw610505@gmail.com>
2025-12-14 17:16:09 +0200

    added getuser from kindeserver to check user in request

--- src/app/thank-you/actions.ts
+++ src/app/thank-you/actions.ts
@@ -1,9 +1,17 @@
 "use server";
 
 import { db } from "../db";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
 export const getPaymentStatus = async ({ orderId }: { orderId: string }) => {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+
+  if (!user?.id || !user.email) {
+    throw new Error("You need to be logged in to view thank-you page.");
+  }
   // Fetch order first to get the user
+
   const order = await db.order.findFirst({
     where: { id: orderId },
     include: {
